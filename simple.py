054e109ee34c371b9ed0de58576a86a9a0bebdd1
breadbread1984 <breadbread1984@163.com>
2021-12-23 16:31:17 +0800

    updat

--- simple.py
+++ simple.py
@@ -9,7 +9,7 @@ class SIMPLE(object):
     self.ny = ny;
     self.nz = nz;
     # domain discretization
-    self.x, self.y, self.z = self.domain_discretization();
+    self.x, self.y, self.z, self.dx, self.dy, self.dz = self.domain_discretization();
     # fluid properties
     self.rho, self.mu = self.fluid_properties();
     # underelaxation properties
@@ -19,11 +19,14 @@ class SIMPLE(object):
     # set conditions 
     self.setConditions();
   def domain_discretization(self,):
-    x = tf.constant([i * 1/self.nx for i in range(self.nx + 1)]); # x in range [0,1] quantized into nx+1 values
-    y = tf.constant([i * 2*pi/(self.ny + 1) for i in range(self.ny + 1)]); # y in range [0, 2 * pi] quantized into ny+1 values
-    z = tf.constant([i * 1/self.nz for i in range(self.nz + 1)]); # z in range [0,1] quantized into nz+1 values
+    dx = 1/self.nx;
+    x = tf.constant([i * dx for i in range(self.nx + 1)]); # x in range [0,1] quantized into nx+1 values
+    dy = 2*pi/(self.ny + 1);
+    y = tf.constant([i * dy for i in range(self.ny + 1)]); # y in range [0, 2 * pi] quantized into ny+1 values
+    dz = 1/self.nz;
+    z = tf.constant([i * dz for i in range(self.nz + 1)]); # z in range [0,1] quantized into nz+1 values
     # NOTE: x.shape = (nx+1,) y.shape = (ny + 1) z.shape = (nz + 1)
-    return x,y,z;
+    return x,y,z, dx,dy,dz;
   def fluid_properties(self,):
     # NOTE: derivable member function
     rho = 1.;
@@ -63,7 +66,7 @@ class SIMPLE(object):
     self.w = tf.concat([self.w[:,-2:-1,:], self.w[:,-2:-1,:], self.w[:,2:-1,:], self.w[:,-2:-1,:]], axis = 1);
   def indices(self, indices_x, indices_y, indices_z, dx = 0, dy = 0, dz = 0):
     return tf.stack([indices_x + dx, indices_y + dy, indices_z + dz], axis = -1);
-  def momento_x(self, u_old, v_old, w_old):
+  def momento_x(self, u_old, v_old, w_old, velocity_iter):
     indices_x = tf.tile(tf.reshape(tf.range(2, self.nx), (-1, 1, 1)), (1, self.ny - 1, self.nz - 1)); # indices_x = 2, ... , nx - 1 has totally nx - 2 numbers
     indices_y = tf.tile(tf.reshape(tf.range(1, self.ny), (1, -1, 1)), (self.nx - 2, 1, self.nz - 1)); # indices_y = 1, ... , ny - 1 has totally ny - 1 numbers
     indices_z = tf.tile(tf.reshape(tf.range(1, self.nz), (1, 1, -1)), (self.nx - 2, self.ny - 1, 1)); # indices_z = 1, ... , nz - 1 has totally nz - 1 numbers
@@ -130,15 +133,54 @@ class SIMPLE(object):
                                                tf.gather_nd(v_old, self.indices(indices_x, self.ny * tf.ones_like(indices_y, dtype = tf.int32), indices_z)));
     flow_south = .5 * self.rho * area_south * (tf.gather_nd(v_old, self.indices(indices_x - 1, tf.ones_like(indices_y, dtype = tf.int32), indices_z)) + \
                                                tf.gather_nd(v_old, self.indices(indices_x, tf.ones_like(indices_y, dtype = tf.int32), indices_z)));
+    tail = tf.math.maximum(-flow_north, 0) + self.mu * area_north / ((tf.gather(self.y, indices_y + 1) - tf.gather(self.y, indices_y)) * tf.gather(self.x, indices_x) / 2);
+    head = tf.math.maximum(flow_south, 0) + self.mu * area_south / ((tf.gather(self.y, tf.ones_like(indices_y)) - tf.gather(self.y, tf.zeros_like(indices_y))) * tf.gather(self.x, indices_x) / 2);
+    An = tf.concat([An[:,:-1,:], tail[:,-1:,:]], axis = 1); # An.shape = (nx-2, ny-1, nz-1)
+    As = tf.concat([head[:,:1,:], As[:,1:,:]], axis = 1); # As.shape = (nx-2, ny-1, nz-1)
     
+    area_top = (tf.gather(self.y, indices_y + 1) - tf.gather(self.y, indices_y)) * \
+               (((tf.gather(self.x, indices_x) + tf.gather(self.x, indices_x + 1))/2)**2 - \
+                ((tf.gather(self.x, indices_x) + tf.gather(self.x, indices_x - 1))/2)**2) / 2;
+    area_bottom = (tf.gather(self.y, indices_y + 1) - tf.gather(self.y, indices_y)) * \
+                  (((tf.gather(self.x, indices_x) + tf.gather(self.x, indices_x + 1))/2)**2 - \
+                   ((tf.gather(self.x, indices_x) + tf.gather(self.x, indices_x - 1))/2)**2) / 2;
+    flow_top = .5 * self.rho * self.dx * self.dy * (tf.gather_nd(w_old, self.indices(indices_x, indices_y - 1, self.nz * tf.ones_like(indices_z, dtype = tf.int32))) + \
+                                                    tf.gather_nd(w_old, self.indices(indices_x, indices_y, self.nz * tf.ones_like(indices_z))));
+    flow_bottom = .5 * self.rho * self.dx * self.dy * (tf.gather_nd(w_old, self.indices(indices_x, indices_y - 1, tf.ones_like(indices_z, dtype = tf.int32))) + \
+                                                       tf.gather_nd(w_old, self.indices(indices_x, indices_y, tf.ones_like(indices_z, dtype = tf.int32))));
+    tail = tf.math.maximum(-flow_top, 0) + self.mu * area_top / ((tf.gather(self.z, indices_z + 1) - tf.gather(self.z, indices_z)) / 2);
+    head = tf.math.maximum(flow_bottom, 0) + self.mu * area_bottom / ((tf.gather(self.z, tf.ones_like(indices_z)) - tf.gather(self.z, tf.zeros_like(indices_z))) / 2);
+    At = tf.concat([At[:,:,:-1], tail[:,:,-1:]], axis = 2); # An.shape = (nx-2, ny-1, nz-1)
+    Ab = tf.concat([head[:,:,:1], Ab[:,:,1:]], axis = 2); # Ab.shape = (nx-2, ny-1, nz-1)
+    # Calculation
+    Apu = (Ae + Aw + An + As + At + Ab) / self.omega_u;
+    # update self.u with iteration
+    for i in range(velocity_iter):
+      dV = -(tf.gather(self.y, indices_y - 1) - tf.gather(self.y, indices_y + 1)) * \
+            (tf.gather(self.z, indices_z - 1) - tf.gather(self.z, indices_z + 1)) * \
+            (2 * tf.gather(self.x, indices_x) + tf.gather(self.x, indices_x - 1) + tf.gather(self.x, indices_x + 1)) * \
+            (tf.gather(self.x, indices_x - 1) - tf.gather(self.x, indices_x + 1)) / 32;
+      dX = (tf.gather(self.x, indices_x + 1) - tf.gather(self.x, indices_x - 1)) / 2;
+      Valor = Ae * tf.gather_nd(self.u, self.indices(indices_x + 1, indices_y, indices_z)) + \
+              Aw * tf.gather_nd(self.u, self.indices(indices_x - 1, indices_y, indices_z)) + \
+              An * tf.gather_nd(self.u, self.indices(indices_x, indices_y + 1, indices_z)) + \
+              As * tf.gather_nd(self.u, self.indices(indices_x, indices_y - 1, indices_z)) + \
+              At * tf.gather_nd(self.u, self.indices(indices_x, indices_y, indices_z + 1)) + \
+              Ab * tf.gather_nd(self.u, self.indices(indices_x, indices_y, indices_z - 1)) - \
+              self.beta * (Dcc - Dcu) + \
+              dV / dX * (tf.gather_nd(self.P, self.indices(indices_x - 1, indices_y, indices_z)) - \
+                         tf.gather_nd(self.P, self.indices(indices_x, indices_y, indices_z)));
+      self.u = (1 - self.omega_u) * u_old + tf.pad(Valor / Apu, [[2,1],[1,1],[1,1]]);
+    Apu = tf.pad(Apu, [[2,1],[1,1],[1,1]]);
+    return Apu;
 
   def momento_y(self,):
     pass;
   def momento_z(self,):
     pass;
-  def solve(self,):
+  def solve(self, iteration = 10, velocity_iter = 10, pressure_iter = 20):
     u_old, v_old, w_old = self.u, self.v, self.w;
-    self.momento_x(u_old, v_old, w_old);
+    Apu = self.momento_x(u_old, v_old, w_old, velocity_iter);
 
 if __name__ == "__main__":
   simple = SIMPLE();
